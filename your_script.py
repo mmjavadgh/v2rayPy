251428c4807a8ff0063f861bdbdb24da43c6f064
mmjavadgh <102357128+mmjavadgh@users.noreply.github.com>
2023-11-24 12:39:40 +0330

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -1,4 +1,3 @@
-import re
 import requests
 
 # تنظیمات
@@ -6,32 +5,27 @@ CHANNEL_USERNAME = "TVCminer"
 GITHUB_REPO = 'https://api.github.com/repos/mmjavadgh/v2rayPy/contents/links.txt'
 GITHUB_TOKEN = "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"
 
-# الگوی شناسایی لینک‌های v2ray
-v2ray_link_pattern = re.compile(r'vmess://[a-zA-Z0-9+/=_-]+')
-
-def get_v2ray_links_from_text(text):
-    # یافتن تمام لینک‌های v2ray در یک متن
-    links = v2ray_link_pattern.findall(text)
-    return links
-
-def get_links_from_channel():
+def get_text_from_channel():
     try:
         # استفاده از تلگرام وب برای دریافت محتوای کانال
         url = f'https://t.me/{CHANNEL_USERNAME}'
         response = requests.get(url)
         
         if response.status_code == 200:
-            # یافتن لینک‌های v2ray از محتوای دریافتی
-            v2ray_links = get_v2ray_links_from_text(response.text)
-            return v2ray_links
+            # گرفتن متن کامل از محتوای دریافتی
+            text = response.text
+            return text
         else:
             print(f"Error: Unable to fetch content from the channel. Status code: {response.status_code}")
-            return []
+            return None
     except Exception as e:
         print(f"An error occurred: {e}")
-        return []
+        return None
 
-def update_github_file(links):
+def update_github_file(text):
+    if text is None:
+        return
+    
     headers = {
         'Authorization': f'token {GITHUB_TOKEN}',
     }
@@ -45,8 +39,8 @@ def update_github_file(links):
 
     current_sha = current_content['sha']
 
-    # ایجاد محتوای جدید با لینک‌های v2ray
-    new_content = f"{'\n'.join(links)}\n"
+    # ایجاد محتوای جدید با متن دریافتی
+    new_content = text
 
     data = {
         'message': 'Update links.txt',
@@ -63,5 +57,5 @@ def update_github_file(links):
         print(f"Error: {response.status_code} - {response.text}")
 
 if __name__ == "__main__":
-    links = get_links_from_channel()
-    update_github_file(links)
+    text = get_text_from_channel()
+    update_github_file(text)
